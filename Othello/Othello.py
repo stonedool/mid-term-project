1ac3f4e449b5e9a52bfb266bfc94c22858ec59fd
stonedool <stonedool@naver.com>
2020-12-05 21:03:01 +0900

    씬3 완성

---
+++ Othello/Othello.py
@@ -0,0 +1,274 @@
+from bangtal import *
+from enum import Enum
+
+setGameOption(GameOption.ROOM_TITLE, False)
+setGameOption(GameOption.INVENTORY_BUTTON, False)
+setGameOption(GameOption.MESSAGE_BOX_BUTTON, False)
+
+Main = Scene('20153182', 'Images/background.png')
+
+class State(Enum):
+    BLANK = 0
+    POSSIBLE = 1
+    BLACK = 2
+    WHITE = 3
+
+class Turn(Enum):
+    BLACK = 0
+    WHITE = 1
+
+stone = []
+turn = Turn.BLACK
+
+Num1 = Object('Images/L0.png')
+Num1.locate(Main, 755, 220)
+Num1.show()
+
+Num2 = Object('Images/L2.png')
+Num2.locate(Main, 825, 220)
+Num2.show()
+
+Num3 = Object('Images/L0.png')
+Num3.locate(Main, 1075, 220)
+Num3.show()
+
+Num4 = Object('Images/L2.png')
+Num4.locate(Main, 1145, 220)
+Num4.show()
+
+def score():
+    blackStone = 0
+    whiteStone = 0
+
+    for y in range(8):
+        for x in range(8):
+            if stone[y][x].state == State.BLACK:
+                blackStone += 1
+            elif stone[y][x].state == State.WHITE:
+                whiteStone += 1
+    Num1.setImage('Images/L' +str(blackStone//10)+ '.png')
+    Num2.setImage('Images/L' +str(blackStone%10)+ '.png')
+    Num3.setImage('Images/L' +str(whiteStone//10)+ '.png')       
+    Num4.setImage('Images/L' +str(whiteStone%10)+ '.png')
+    
+def setState(x, y, state):
+    object = stone[y][x]
+    object.state = state
+
+    if state == State.BLANK:
+        object.setImage("Images/blank.png")
+    elif state == State.BLACK:
+        object.setImage("Images/black.png")
+    elif state == State.WHITE:
+        object.setImage("Images/white.png")
+    elif turn == Turn.BLACK:
+        object.setImage("Images/black possible.png")
+    else:
+        object.setImage("Images/white possible.png")
+        
+def stone_onMouseAction(x, y):
+    global turn
+
+    object = stone[y][x]
+
+    if object.state == State.POSSIBLE:
+        if turn == Turn.BLACK:
+            setState(x, y, State.BLACK)
+            reverse_xy(x, y)
+            turn = Turn.WHITE
+            
+        else:
+            setState(x, y, State.WHITE)
+            reverse_xy(x, y)
+            turn = Turn.BLACK
+
+    if not setPossible():
+        if turn == Turn.BLACK:
+            turn = Turn.WHITE
+        else:
+            turn = Turn.BLACK
+
+        if not setPossible():
+            showMessage("게임이 종료되었습니다.")
+    
+    score()
+    computer()
+    
+
+
+
+def setPossible_xy_dir(x, y, dx, dy):
+    if turn == Turn.BLACK:
+        mine = State.BLACK
+        other = State.WHITE
+    
+    else:
+        mine = State.WHITE
+        other = State.BLACK
+
+    possible = False
+
+    while True:
+        x = x + dx
+        y = y + dy
+
+        if x < 0 or x > 7: return False
+        if y < 0 or y > 7: return False
+
+        object = stone[y][x]
+        if object.state == other:
+            possible = True
+        elif object.state == mine:
+            return possible # False?
+        else:
+            return False
+
+def setPossible_xy(x, y):
+    object = stone[y][x]
+    if object.state == State.BLACK: return False
+    if object.state == State.WHITE: return False
+    setState(x, y, State.BLANK)
+
+    if (setPossible_xy_dir(x, y, 0, 1)): return True
+    if (setPossible_xy_dir(x, y, 1, 1)): return True
+    if (setPossible_xy_dir(x, y, 1, 0)): return True
+    if (setPossible_xy_dir(x, y, 1, -1)): return True
+    if (setPossible_xy_dir(x, y, 0, -1)): return True
+    if (setPossible_xy_dir(x, y, -1, -1)): return True
+    if (setPossible_xy_dir(x, y, -1, 0)): return True
+    if (setPossible_xy_dir(x, y, -1, 1)): return True
+    return False
+
+def setPossible():
+    possible = False
+
+    for y in range(8):
+        for x in range(8):
+            if setPossible_xy(x, y):
+                setState(x, y, State.POSSIBLE)
+                possible = True
+
+    return possible
+
+def reverse_xy_dir(x, y, dx, dy):
+    if turn == Turn.BLACK:
+        mine = State.BLACK
+        other = State.WHITE
+    
+    else:
+        mine = State.WHITE
+        other = State.BLACK
+
+    possible = False
+
+    while True:
+        x = x + dx
+        y = y + dy
+
+        if x < 0 or x > 7: return
+        if y < 0 or y > 7: return
+
+        object = stone[y][x]
+        if object.state == other:
+            possible = True
+        elif object.state == mine:
+            if possible:
+                while True:
+                    x = x - dx
+                    y = y - dy
+
+                    object = stone[y][x]
+                    if object.state == other:
+                        setState(x, y, mine)
+                    else:
+                        return
+        else:
+            return
+
+
+def reverse_xy(x, y):
+    reverse_xy_dir(x, y, 0, 1)
+    reverse_xy_dir(x, y, 1, 1)
+    reverse_xy_dir(x, y, 1, 0)
+    reverse_xy_dir(x, y, 1, -1)
+    reverse_xy_dir(x, y, 0, -1)
+    reverse_xy_dir(x, y, -1, -1)
+    reverse_xy_dir(x, y, -1, 0)
+    reverse_xy_dir(x, y, -1, 1)
+    return False
+
+
+def count_xy_dir(x, y, dx, dy):
+    count = 0
+    if turn == Turn.BLACK:
+        mine = State.BLACK
+        other = State.WHITE
+    
+    else:
+        mine = State.WHITE
+        other = State.BLACK
+
+    while True:
+        x = x + dx
+        y = y + dy
+
+        if x < 0 or x > 7: return 0
+        if y < 0 or y > 7: return 0
+
+        object = stone[y][x]
+        if object.state == other:
+            count = count + 1
+        elif object.state == mine:
+            return count
+        else:
+            return 0
+
+def count_xy(x, y):
+    return 1 + count_xy_dir(x, y, 0, 1) 
+    + count_xy_dir(x, y, 1, 1)
+    + count_xy_dir(x, y, 1, 0)
+    + count_xy_dir(x, y, 1, -1)
+    + count_xy_dir(x, y, 0, -1)
+    + count_xy_dir(x, y, -1, -1)
+    + count_xy_dir(x, y, -1, 0)
+    + count_xy_dir(x, y, -1, 1)
+
+
+def computer():
+    max = 0
+    if turn == Turn.WHITE:
+        for y in range(8):
+            for x in range(8):
+                if stone[y][x].state == State.POSSIBLE:
+                    count = count_xy(x, y)
+                    if count > max:
+                        max = count
+                        Mx = x
+                        My = y
+
+    stone_onMouseAction(Mx, My)
+
+                    
+
+
+for y in range(8):
+    stone.append([])
+
+    for x in range(8):
+        object = Object('Images/blank.png')
+        object.locate(Main, 40 + x*80, 40 + y*80)
+        object.state = State.BLANK
+        object.show()
+        
+        object.onMouseAction = lambda mx, my, action, ix = x, iy = y: stone_onMouseAction(ix, iy)
+
+        stone[y].append(object)
+
+
+
+setState(3, 3, State.BLACK)
+setState(3, 4, State.WHITE)
+setState(4, 3, State.WHITE)
+setState(4, 4, State.BLACK)
+
+startGame(Main)
--- mid_term_project.py
+++ mid_term_project.py
@@ -1,15 +1,18 @@
 from bangtal import *
+from Othello import *
 
 setGameOption(GameOption.INVENTORY_BUTTON, False)
 setGameOption(GameOption.MESSAGE_BOX_BUTTON, False)
 
 #Scene 
-Rat_House_Scene = Scene('집', 'images/Rat_House.jpg')
+Rat_House_Scene = Scene('생쥐의 집', 'images/Rat_House.jpg')
 scene2 = Scene('집 앞', 'images/House_Scene2.jpg')
-scene3 =Scene('주방 앞', 'images/House_Scene3.jpg')
+scene3 = Scene('주방 앞', 'images/House_Scene3.jpg')
+scene4 = Scene('주방', 'images/House_Scene4.jpg')
 
 #Object
 Rat = Object("images/MainChar1.png")
+Cat = Object("images/Cat.png")
 bed = Object("images/RatBed.png")
 Flower_pot = Object("images/Flower_Pot.png")
 Flower_Pot_2 = Object("images/Flower_Pot.png")
@@ -17,7 +20,7 @@ Flower_Pot_3 = Object("images/Flower_Pot.png")
 Map_1 = Object("images/Map.png")
 Mini_Map = Object("images/Mini_Map.png")
 Desk = Object("images/Desk.png")
-
+GG = Object("GG.png")
 
 #trap (in Scene3)
 Trap_1 = Object("images/Trap.png")
@@ -38,7 +41,7 @@ Rat.y = 300
 Rat.scene = 1
 Rat.scene_STAY = False
 
-#Scene1 (rat house 관련 오브젝트 
+#Scene1 
 Desk.locate(Rat_House_Scene, 400, 200)
 bed.locate(Rat_House_Scene,800,100)
 Flower_pot.locate(Rat_House_Scene,100,100)
@@ -65,8 +68,14 @@ Trap_3.locate(scene3,600,450)
 Trap_4.locate(scene3,100,500)
 Trap_5.locate(scene3,150,400)
 Flower_Pot_3.locate(scene3,100,100); Flower_Pot_3.setScale(0.8);
+GG.locate(scene3, 250, 100)
+
+#Scene4 관련 
+Cat.locate(scene4,600,100)
 
 
+#object show
+Cat.show()
 Desk.show()
 bed.show()
 Mini_Map.show()
@@ -141,7 +150,7 @@ def down_press(x,y,action):
 
 #위 네가지 코드가 캐릭터의 이동을 결정합니다.
 #각 씬별 elif  케이스는 손을 좀 봐야 할것 같습니다. 
-# next scene을 위한 함수가 필요합니다. 
+
 
 def NextScene(): # 씬체인지 기능입니다. 
     if Rat.scene == 1:
@@ -154,13 +163,17 @@ def NextScene(): # 씬체인지 기능입니다.
                     Rat.locate(scene2, Rat.x, Rat.y)
                     Switch_ReLocate(scene2) # 방향키가 해당 씬에도 나타날수 있게 하는 함수입니다. 
                     Rat.scene_STAY = True
+                    Mini_Map.locate(scene2, 1100, 600);
+                    Map_1.locate(scene2,200,100)
         elif Rat.scene_STAY == True:
             Scene_Stay(150,240,300,450) #씬 체인지 이후에 stay중인지 이동하여 해당 구간을 벗어났는지를 판별합니다. 
 
     elif Rat.scene == 2:
-        if Rat.scene_STAY == False: #false 는 생쥐가 기존 씬에서 넘어온 후 다시 원래의 씬으로 돌아가지 않게 하는 변수입니다.
-                                    #해당 구간에 있을때는 true, 나가면 false로 설정합니다. 
-            if Rat.x >= 610 and Rat.x <= 760:
+        if Rat.scene_STAY == False: 
+            #false 는 생쥐가 기존 씬에서 넘어온 후 다시 원래의 씬으로 돌아가지 않게 하는 변수입니다.
+            #해당 구간에 있을때는 true, 나가면 false로 설정합니다. 
+
+            if Rat.x >= 610 and Rat.x <= 760: # scene1 진입코드 
                 if Rat.y >= 420 and Rat.y <= 520:
                     Rat_House_Scene.enter()
                     Rat.scene = 1
@@ -168,16 +181,43 @@ def NextScene(): # 씬체인지 기능입니다.
                     Rat.locate(Rat_House_Scene, Rat.x, Rat.y)
                     Switch_ReLocate(Rat_House_Scene)
                     Rat.scene_STAY = True
+                    Mini_Map.locate(Rat_House_Scene, 1100, 600);
+                    Map_1.locate(Rat_House_Scene,200,100)
+
+            if Rat.x <= 30:
+                    scene3.enter() # scene 3 진입코드 
+                    Rat.scene = 3
+                    Rat.x = 1100; Rat.y = 60;
+                    Rat.locate(scene3,Rat.x,Rat.y)
+                    Switch_ReLocate(scene3)
+                    Rat.scene_STAY = True
+                    Mini_Map.locate(scene3, 1100, 600);
+                    Map_1.locate(scene3,200,100)
         elif Rat.scene_STAY == True:
-            Scene_Stay(610,760,420,520)
+            if  Rat.x > 200:
+                Scene_Stay(610,760,420,520) #scene stay Gate1 
+            else:
+                Scene_Stay(0, 100, 50, 120) #scene stay Gate3
         
         pass
     elif Rat.scene == 3:
+        if Rat.scene_STAY == False: #scene2 진입코드 
+            if Rat.x > 1230:
+                scene2.enter()
+                Rat.scene = 2
+                Rat.x = 60; Rat.y = 100;
+                Rat.locate(scene2, Rat.x, Rat.y)
+                Switch_ReLocate(scene2)
+                Rat.scene_STAY = True
+                Mini_Map.locate(scene2, 1100, 600);
+                Map_1.locate(scene2,200,100)
+        elif Rat.scene_STAY == True:
+            Scene_Stay(1050, 1200,40, 70 )
         pass
     elif Rat.scene == 4:
         pass
 
-    #3과 4는 이미지를 만들고 넣겠습니다. 
+    #4 수정 필요. 
 def Switch_ReLocate(SceneName): # 방향키가 해당 씬에도 나타날수 있게 하는 함수입니다. 
 
     up.locate(SceneName, 1000,120)
@@ -191,21 +231,25 @@ def On_Trap():
     #print문을 이벤트로 바꿀 예정입니다. 
     if Rat.scene == 3: 
         #트랩이 있는 스테이지는 stage 3뿐이므로 이를 제외한 맵에서는 검사를 하지 않습니다.
-        if Rat.x >= 500 and Rat.x <= 500+trapSize_x:
+        if Rat.x >= 450 and Rat.x <= 450+trapSize_x:
             if Rat.y >= 100 and Rat.y <= 100+trapSize_y:
-                print("Rat Die!!!")
-        elif Rat.x >= 700 and Rat.x <= 700+trapSize_x:
+                GG.show()
+                debug()
+        elif Rat.x >= 650 and Rat.x <= 650+trapSize_x:
             if Rat.y >= 400 and Rat.y <= 400+trapSize_y:
-                print("Rat Die!!!")
-        elif Rat.x >= 600 and Rat.x <= 600+trapSize_x:
-            if Rat.y >= y and Rat.y <= y+trapSize_y:
-                print("Rat Die!!!")
-        elif Rat.x >= 100 and Rat.x <= 100+trapSize_x:
+                GG.show()
+                debug()
+        elif Rat.x >= 550 and Rat.x <= 550+trapSize_x:
+            if Rat.y >= 450 and Rat.y <= 450+trapSize_y:
+                GG.show()
+                debug()
+        elif Rat.x >= 50 and Rat.x <= 50+trapSize_x:
             if Rat.y >= 500 and Rat.y <= 500+trapSize_y:
-                print("Rat Die!!!")
-        elif Rat.x >= 150 and Rat.x <= 150+trapSize_x:
+                GG.show()
+                debug()
+        elif Rat.x >= 100 and Rat.x <= 100+trapSize_x:
             if Rat.y >= 400 and Rat.y <= 400+trapSize_y:
-                print("Rat Die!!!")
+                GG.show()
 
 
 def Mini_Map_click(x,y,action): #화면 우측 위에 나오는 지도를 띄우기 위한 함수입니다. 클릭하면 지도가 열립니다. 
@@ -214,7 +258,7 @@ def Mini_Map_click(x,y,action): #화면 우측 위에 나오는 지도를 띄우
 def Map_1_click(x,y,action): #지도를닫기 위한 함수입니다. 클릭하면 지도가 닫힙니다. 
     Map_1.hide()
 
-def GG_click(x,y,action): #미구현 기능입니다. 게임이 끝났다는것을 알리기 위한 기능입니다. 
+def GG_click(x,y,action): # 게임이 끝났다는것을 알리기 위한 기능입니다. 
     endGame()
 
 def Scene_Stay(x1,x2,y1,y2):
@@ -228,7 +272,7 @@ down.onMouseAction = down_press
 right.onMouseAction = right_press
 Mini_Map.onMouseAction = Mini_Map_click
 Map_1.onMouseAction = Map_1_click
-#gg.onMouseAction = GG_click
+GG.onMouseAction = GG_click
 
 def debug():
     if Rat.scene_STAY == True:
